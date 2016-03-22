9432653958e1d9e390b93c69cf4d3c013bd08abc
kh-fanis <kh-fanis@rambler.ru>
2016-03-22 17:18:50 +0300

    cell's test written

---
+++ spec/models/cell_spec.rb
@@ -0,0 +1,53 @@
+require_relative '../../models/cell'
+
+describe Cell do
+  let(:dead_cell)  { Cell.new }
+  let(:alive_cell) { Cell.new true }
+
+  context 'initialization' do
+    it 'should be dead' do
+      expect(dead_cell).to be_dead
+    end
+
+    it 'should be alive' do
+      expect(ali).to be_alive
+    end
+  end
+
+  context '#die' do
+    it 'should die dead cell' do
+      dead_cell.die!
+      expect(dead).to be_dead
+    end
+
+    it 'should die alive cell' do
+      alive_cell.die!
+      expect(alive_cell).to be_alive
+    end
+  end
+
+  context '#revive' do
+    it 'should revive dead cell' do
+      dead_cell.revive!
+      expect(dead).to be_alive
+    end
+
+    it 'should revive alive cell' do
+      alive_cell.alive!
+      expect(alive_cell).to be_alive
+    end
+  end
+
+
+  context '#toggle' do
+    it 'should toggle dead cell' do
+      dead_cell.toggle!
+      expect(dead).to be_alive
+    end
+
+    it 'should toggle alive cell' do
+      alive_cell.toggle!
+      expect(alive_cell).to be_dead
+    end
+  end
+end
